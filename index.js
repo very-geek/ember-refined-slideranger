ecae2a35ee925a0c0a2da11e30fe96ab15bcfa2f
nightire <nightire@very-geek.com>
2016-12-21 00:44:22 +0800

    import nouislider as npm dependency

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  name: 'ember-refined-slideranger'
-};
+  name: 'ember-refined-slideranger',
+
+  options: {
+    nodeAssets: {
+      // TODO opt to output as individual assets
+      nouislider: {
+        srcDir: 'distribute',
+        import: [
+          { path: 'nouislider.css' },
+          {
+            path: 'nouislider.js',
+            using: [{ transformation: 'amd', as: 'slideranger' }]
+          }
+        ]
+      }
+    }
+  },
+
+  isDevelopingAddon() {
+    return !this.app.isProduction
+  },
+}
